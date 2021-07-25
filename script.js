edbcce528b2642972e9e5e3cec48f80595001291
Amandeep <vivkkumarkuldep@gamil.com>
2021-07-25 12:21:20 +0530

    toddle task
    task one toddle

---
+++ script.js
@@ -0,0 +1,93 @@
+
+var app = new function() {
+    this.el = document.getElementById('tasks');
+  
+    this.tasks = [];
+  
+    
+    
+    this.FetchAll = function() {
+      var data = '';
+  
+      if (this.tasks.length > 0) {
+        for (i = 0; i < this.tasks.length; i++) {
+          data += '<tr>';
+          data += '<td><button onclick="app.Edit(' + i + ')"  class="btn btn-warning">Edit</button></td>';
+          data += '<td><button onclick="app.Delete(' + i + ')" ><i class="material-icons">delete</i></button></td>';
+       
+          data += '<td>'+(i+1)+". " + this.tasks[i] + '</td>';
+          data += '</tr>';
+        }
+      }
+  
+      this.Count(this.tasks.length);
+      return this.el.innerHTML = data;
+    };
+  
+    this.Add = function () {
+      el = document.getElementById('add-todo');
+      // Get the value
+      var task = el.value;
+  
+      if (task) {
+        // Add the new value
+        this.tasks.push(task.trim());
+        // Reset input value
+        el.value = '';
+        // Dislay the new list
+        this.FetchAll();
+      }
+    };
+  
+    this.Edit = function (item) {
+      var el = document.getElementById('edit-todo');
+      // Display value in the field
+      el.value = this.tasks[item];
+      // Display fields
+      document.getElementById('edit-box').style.display = 'block';
+      self = this;
+  
+      document.getElementById('save-edit').onsubmit = function() {
+        // Get value
+        var task = el.value;
+  
+        if (task) {
+          // Edit value
+          self.tasks.splice(item, 1, task.trim());
+          // Display the new list
+          self.FetchAll();
+          // Hide fields
+          CloseInput();
+        }
+      }
+    };
+  
+    this.Delete = function (item) {
+      // Delete the current row
+      this.tasks.splice(item, 1);
+      // Display the new list
+      this.FetchAll();
+    };
+  
+    this.Count = function(data) {
+      var el   = document.getElementById('counter');
+      var name = 'Message';
+  
+      if (data) {
+          if(data ==1){
+              name = 'Message'
+          }
+        el.innerHTML = data + ' ' + name ;
+      } 
+      else {
+        el.innerHTML = 'No ' + name;
+      }
+    };
+    
+  }
+  
+  app.FetchAll();
+  
+  function CloseInput() {
+    document.getElementById('edit-box').style.display = 'none';
+  }
